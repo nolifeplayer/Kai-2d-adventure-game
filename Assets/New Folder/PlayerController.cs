d08b6b361b048a2b34553cf2474a5eb6a78c5808
nolifeplayer <KAig19@nycstudents.net>
2024-10-16 09:22:41 -0400

    PlayerMovement

--- Assets/New Folder/PlayerController.cs
+++ Assets/New Folder/PlayerController.cs
@@ -4,41 +4,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    public InputAction LeftAction;
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeftAction.Enable();  
+        MoveAction.Enable();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal= 0.0f; 
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal= -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal= 1.0f;
-        }
-        Debug.Log(horizontal);
-       
-        float vertical= 0.0f;
-        if(Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical= -1.0f;
-        }
-        else if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical= 1.0f;
-        }
-        Debug.Log(vertical);
-    
-        Vector2 position = transform.position;
-        position.y = position.y + 0.01f * vertical;
-        position.x=position.x+ 0.01f * horizontal;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
         
     }
