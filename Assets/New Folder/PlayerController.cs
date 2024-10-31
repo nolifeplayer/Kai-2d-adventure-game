22605efbd0000ad287a56ae816cf4f70a262cd3b
nolifeplayer <KAig19@nycstudents.net>
2024-10-31 09:01:23 -0400

    Collider

--- Assets/New Folder/PlayerController.cs
+++ Assets/New Folder/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
 {   
     //public InputAction LeftAction;
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    
     // Start is called before the first frame update
     void Start()
     {
         //LeftAction.Enable();  
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 380;  
     }
@@ -19,10 +23,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+       
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
         
     }
 }
